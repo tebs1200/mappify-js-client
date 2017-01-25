e1caab3b200950ecc5ade967f0bf1aff3ddb0615
Aaren Tebbutt <aaren.tebbutt@noshjournal.com>
2017-01-25 19:56:21 +1100

    Added reverse geocode tests

--- test/reverse-geocode.js
+++ test/reverse-geocode.js
@@ -9,4 +9,61 @@ describe("reverseGeocode", () => {
         expect(mappify.reverseGeocode).to.exist;
     });
 
+    it("should return with error if the first argument isn't a number", (done) => {
+        mappify.reverseGeocode("abc", 153.027, null, (err) => {
+            expect(err).to.exist;
+            expect(err.message).to.equal("Invalid latitude value");
+            done();
+        });
+    });
+
+    it("should return with error if the latitude value is out of range", (done) => {
+        mappify.reverseGeocode(200, 153.027, null, (err) => {
+            expect(err).to.exist;
+            expect(err.message).to.equal("Latitude is out of range");
+            done();
+        });
+    });
+
+    it("should return with error if the second argument isn't a number", (done) => {
+        mappify.reverseGeocode(-27.471, "xyz", null, (err) => {
+            expect(err).to.exist;
+            expect(err.message).to.equal("Invalid longitude value");
+            done();
+        });
+    });
+
+    it("should return with error if the longitude value is out of range", (done) => {
+        mappify.reverseGeocode(-27.471, 300, null, (err) => {
+            expect(err).to.exist;
+            expect(err.message).to.equal("Longitude is out of range");
+            done();
+        });
+    });
+
+    it("should return with error if the third argument is provided but isn't a number", (done) => {
+        mappify.reverseGeocode(-27.471, 153.027, "cheese", (err) => {
+            expect(err).to.exist;
+            expect(err.message).to.equal("Invalid radius value");
+            done();
+        });
+    });
+
+    it("should return with error if the radius value is out of range", (done) => {
+        mappify.reverseGeocode(-27.471, 153.027, -10, (err) => {
+            expect(err).to.exist;
+            expect(err.message).to.equal("Radius is out of range");
+            done();
+        });
+    });
+
+    it.skip("should return a response object for a valid encoding and coordinates", (done) => {
+        mappify.reverseGeocode(-27.471, 153.027, null, (err, res) => {
+            expect(err).not.to.exist;
+            expect(res).to.exist;
+            expect(res.type).to.equal("streetAddressRecord");
+            done();
+        });
+    });
+
 });
